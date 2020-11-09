c89356a3f609b13d9a3e022dc3371583b2aedb2b
noga <nogaarzuan@gmail.com>
2020-11-09 13:46:18 +0200

    1.1 add popular movies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+// import Movies from "./components/Movies";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends React.Component {
+   constructor (props){
+      super(props)  
+      this.state=({value:"",data:[]})
+   }
+      handleOnClick = async() => {
+    
+            
+            const fetchData = await axios.get( 'https://api.themoviedb.org/3/movie/popular?api_key=7ba170f1fa3f9013d4dbd05771244665&language=en-US&page=1');
+      this.setState({ data: fetchData.data.results});
+      console.log(fetchData.data.results);
+      
+      };
+  
+      render(){
+        const renderMovie  = () => {
+            return   this.state.data.map((movie) => {
+              console.log(movie);
+            return <div> <h1>"{movie.title}" </h1>
+        
+           </div>
+            })};
+         
+        
+          return <div>
+          <button className="button" onClick={this.handleOnClick}>Get A list</button>
+          {this.state.data && renderMovie()}  
+      </div>
+                }}
+            
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders click me link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/click me/i);
   expect(linkElement).toBeInTheDocument();
 });
